f6e5a9a02874aac5d72cdfa396ab395751159c98
FahUnji <tunyaluck.kt@gmail.com>
2017-07-21 02:45:14 +0700

    edit pin name

--- dht22.py
+++ dht22.py
@@ -11,12 +11,12 @@ GPIO.setup(yellow, GPIO.OUT)
 GPIO.setup(blue, GPIO.OUT)
 
 sensor = Adafruit_DHT.DHT22
-pin = 4
+dhtpin = 4
 temp = 0
 humi = 0
 
 while True:
-  humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
+  humidity, temperature = Adafruit_DHT.read_retry(sensor, dhtpin)
   if humidity is not None and temperature is not None:
 	print 'Temp={0:0.1f}*C  Humidity={1:0.1f}%'.format(temperature, humidity)
 	if temp != temperature and humi != humidity:
